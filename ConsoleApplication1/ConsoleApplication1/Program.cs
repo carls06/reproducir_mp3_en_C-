26079fd13255b0b45fe201755c01749bf6aac146
carlos <contactos503@gmail.com>
2019-03-19 10:42:40 -0600

    Reproducir .mp3 en c#

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 
+//para que pueda reproducir un mp3 hay que agregar una referencia wmp.dll 
+//la referencia wmp.dll esta en C:\Windows\    en esa direccion se puede buscar 
+//
+//despues de agregar la referencia se puede agregar la libreria siguiente 
+using WMPLib;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            WindowsMediaPlayer musica = new WindowsMediaPlayer();
+
+            //el archivo mp3 debe estar en la carpeta del proyecto
+            musica.URL = "C:\\Users\\pc\\Desktop\\ConsoleApplication\\sound.mp3";
+
+        
+                Console.WriteLine("sonido");
+                musica.controls.play();
+           
+            
+            Console.ReadKey();
+        }
+    }
+}
